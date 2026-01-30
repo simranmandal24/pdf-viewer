103e5cb59b6e40d1ae628393a8ca82e03e145fb6
simranmandal24 <simranmandal24@navgurukul.org>
2026-01-30 11:58:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PdfdragView from "./components/PdfdragView";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>PDF Viewer</h1>
+      <PdfdragView />
+    </div>
+  );
+}
